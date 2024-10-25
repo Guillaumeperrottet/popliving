149b5175d7fe3ad7475f21226ee76287b6b7a9b6
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-25 15:04:40 +0200

    envoi de mail

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,8 +1,8 @@
 class BookingMailer < ApplicationMailer
-  default from: 'no-reply@tonsite.com'
+  default from: ENV.fetch('EMAIL_FROM', 'no-reply@tonsite.com') # Utilise l'adresse par défaut si la variable n'est pas définie
 
   def new_booking(booking)
     @booking = booking
-    mail(to: 'info@campus-gerance.ch', subject: 'PopLiving Riaz - Demande de Réservation')
+    mail(to: ENV.fetch('RESERVATION_EMAIL', 'info@campus-gerance.ch'), subject: 'PopLiving Riaz - Demande de Réservation')
   end
 end
