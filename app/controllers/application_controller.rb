d82dad9c2bf6f15a79a0064db413e46a1e44ea2d
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-25 13:53:43 +0200

    Ajout d'une page de maintenance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :show_maintenance
+
+  private
+
+  def show_maintenance
+    # Vérifie si l'environnement est en production pour éviter la redirection en développement
+    if Rails.env.production?
+      render file: "#{Rails.root}/public/maintenance.html", layout: false
+    end
+  end
 end
