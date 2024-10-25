ca7804c4ef174512dcd0283c776e21124a3d7aac
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-10-25 14:40:44 +0200

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: ENV['EMAIL_USER_NAME'],
-    password: ENV['EMAIL_PASSWORD'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
     authentication: "plain",
     enable_starttls_auto: true
   }
