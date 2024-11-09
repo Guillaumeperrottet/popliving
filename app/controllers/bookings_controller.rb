c695f8f445bcd814bb3a123a02b49d0935c96e54
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2024-11-09 13:13:55 +0100

    1er test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,12 +6,15 @@ class BookingsController < ApplicationController
       # Envoie un email avec les détails de la réservation
       puts "Booking successfully saved: #{@booking.inspect}"  # Message de confirmation
 
+      # Envoie l'email de confirmation
+      BookingMailer.confirmation_email(@booking).deliver_now
+
       BookingMailer.new_booking(@booking).deliver_now
-      redirect_to new_booking_path, notice: "Votre demande a bien été envoyée."
+      redirect_to new_booking_path, notice: I18n.t('reservation.success')
     else
       puts "Booking failed to save: #{@booking.errors.full_messages}"  # Affiche les erreurs de validation
 
-      flash.now[:alert] = "La réservation n'a pas pu être enregistrée. Veuillez corriger les erreurs ci-dessous."
+      flash.now[:alert] = I18n.t('reservation.failure')
       render "pages/booking", status: :unprocessable_entity
     end
   end
